67adc7a55eae71d6401a311e78c2a77b4d6d2b5c
Giuliana Domínguez <118238887+giulianadominguez@users.noreply.github.com>
2023-03-01 20:27:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const acordeon = document.getElementsByClassName('pregunta');
-
-for (i=0; i<acordeon.length; i++) {
-  acordeon[i].addEventListener('click', function () {
-    this.classList.toggle('activa')
-  })
-}
